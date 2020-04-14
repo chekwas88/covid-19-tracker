5501d27633c71772e075328d1609bb1b5f42e8aa
chisom onwuchekwa <princechekwas@gmail.com>
2020-04-14 11:29:36 +0100

    --amend

---
+++ build/precache-manifest.8882e85b621c9ca97f518256241ad267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c68044d8c528ee63848e909a09b09c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f83d71697778b140fd",
+    "url": "/static/css/main.2607617b.chunk.css"
+  },
+  {
+    "revision": "02680a5b61c812397215",
+    "url": "/static/js/2.bb2cea42.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.bb2cea42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f83d71697778b140fd",
+    "url": "/static/js/main.c3a9edbe.chunk.js"
+  },
+  {
+    "revision": "63f4e8b58fac8513d449",
+    "url": "/static/js/runtime-main.310cdbaa.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/corona.d7265326.png"
+  }
+]);
